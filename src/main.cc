922d17133568c8830458c08665ec6e993588f4bb
danielstruck <struckd@my.erau.edu>
2018-03-21 16:33:38 -0700

    fixed typo in main.cc NOTE: image::write does not work

--- src/main.cc
+++ src/main.cc
@@ -6,13 +6,13 @@
 using namespace png;
 
 int main() {
-    typedef ty
     image<rgb_pixel> img;
     
     // apply some gradient
     
     
     // save the image
+    using namespace std;
     string outputFile;
     cout << "output file:";
     cin >> outputFile;
