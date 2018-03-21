07b2cf8cb38714f7396eedac3fd556d96dab274d
danielstruck <struckd@my.erau.edu>
2018-03-21 16:26:50 -0700

    created an image object in main.cc and added code to save the generated gradient to the file specified by the user

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,20 @@
 #include "../vendor/png++-0.2.9/png.hpp"
 #include "GradientAlgorithm.h"
 
+using namespace png;
+
 int main() {
-    std::cout<<"hello"<<std::endl;
+    typedef ty
+    image<rgb_pixel> img;
+    
+    // apply some gradient
+    
+    
+    // save the image
+    string outputFile;
+    cout << "output file:";
+    cin >> outputFile;
+    img.write(outputFile);
+    
     return 0;
 }
